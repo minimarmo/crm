32a82dcb5853f6e839c601da6a31c734b3521fe2
marmo <kaew.renuka@gmail.com>
2024-10-22 15:04:08 +0700

    set config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/crm", // แทนที่ <REPO_NAME> ด้วยชื่อ repository ของคุณ
+  assetPrefix: "/crm/", // ทำให้สามารถเรียก asset ได้จาก basePath
+  reactStrictMode: true,
+};
 
 export default nextConfig;
